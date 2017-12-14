81fb6e9c540b2d7e22bd48c9fc40df44ae5961c3
jxmot <webdev@stupiddog.net>
2017-12-14 03:40:21 -0600

    comments

--- bar-sign/src/applib/nodejs/multi-udp-cfg.js
+++ bar-sign/src/applib/nodejs/multi-udp-cfg.js
@@ -1,5 +1,8 @@
 /*
     UDP Multi-Cast Client Configuration
+
+    IP addresses in the range 224.0.0.0 through 
+    239.255.255.255 are reserved for multicasting.
 */
 module.exports = {
     addr : '224.0.0.1',

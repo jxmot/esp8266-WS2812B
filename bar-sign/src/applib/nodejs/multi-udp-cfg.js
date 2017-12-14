f005c3968bc167fe8f5e56ab1271914dbe0f7d29
jxmot <webdev@stupiddog.net>
2017-12-14 03:35:24 -0600

    added udp multicast config and nodejs test

---
+++ bar-sign/src/applib/nodejs/multi-udp-cfg.js
@@ -0,0 +1,8 @@
+/*
+    UDP Multi-Cast Client Configuration
+*/
+module.exports = {
+    addr : '224.0.0.1',
+    port : 54321,
+};
+
---
+++ bar-sign/src/applib/nodejs/multi-udp.js
@@ -0,0 +1,43 @@
+/* ************************************************************************ */
+/*
+    multi-udp.js - 
+
+*/
+// an option argument can specify and alternative client configuration file. 
+var clientCfgFile = process.argv[2];
+
+// we're assuming that if there's an argument present then it's going to be
+// "path + filename.ext". It's not checked for validity.
+if((clientCfgFile === undefined) || (clientCfgFile === ''))
+    clientCfgFile = './multi-udp-cfg.js';
+
+// read the IP address and port # that we'll be using
+const cfg = require(clientCfgFile);
+const addr = cfg.addr;
+const port = cfg.port;
+
+// create a socket to listen on...
+const dgram = require('dgram');
+const client = dgram.createSocket('udp4');
+
+client.on('listening', function () {
+      client.setBroadcast(true)
+      client.setMulticastTTL(128); 
+      client.addMembership(addr);
+
+      var address = client.address();
+      //console.log('UDP Client listening on ' + address.address + ":" + address.port);
+      console.log('UDP Client listening on ' + addr + ":" + port);
+});
+
+/*
+    Reply Received Event Handler
+*/
+client.on('message', (payload, remote) => {
+
+    // the correct way to extract a string from the payload is this - 
+    var message = payload.filter(letter => letter !== 0);
+    console.log(`multicast received : [${message}] from ${remote.address}:${remote.port}`);
+});
+
+client.bind(port);

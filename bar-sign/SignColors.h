9a9362ad945c1e1a98072afb29b5067d10f0caff
jxmot <webdev@stupiddog.net>
2017-12-19 14:09:36 -0600

    added MAKEHSL() macro

--- bar-sign/SignColors.h
+++ bar-sign/SignColors.h
@@ -18,6 +18,7 @@ enum ColorSatLevel
 #define COLOR_BLACK RgbColor(colorSat_0, colorSat_0, colorSat_0)
 
 #define MAKERGB(rgb) RgbColor(rgb[0], rgb[1], rgb[2])
+#define MAKEHSL(hsl) HslColor((hsl[0] * 0.002777), (hsl[1] * 0.01), (hsl[2] * 0.005))
 
 #define DEFAULT_COLOR COLOR_BLACK
 
